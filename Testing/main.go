bab0614da52613c9a4cbfad98a9e50e27c9026de
Gad <objetunique@gmail.com>
2025-03-03 23:33:40 +0100

    Added Pointers chapter + formatting

--- Testing/main.go
+++ Testing/main.go
@@ -6,42 +6,42 @@ import (
 )
 
 var (
-	ErrNotImplemented = errors.New("Not implemented")
-	ErrTruckNotFound  = errors.New("Truck Not Found")
+	ErrNotImplemented = errors.New("not implemented")
+	ErrTruckNotFound  = errors.New("truck Not Found")
 )
 
-type Truck interface{
+type Truck interface {
 	LoadCargo() error
 	UnloadCargo() error
 }
 
 type NormalTruck struct {
-	Id string
+	Id    string
 	cargo int
 }
+
 func (t *NormalTruck) LoadCargo() error {
 	t.cargo += 2
 	return nil
 }
 func (t *NormalTruck) UnloadCargo() error {
-	t.cargo =0
+	t.cargo = 0
 	return nil
 }
 
-
-
 type ElectricTruck struct {
-	Id string
-	cargo int
+	Id      string
+	cargo   int
 	battery float64
 }
+
 func (e *ElectricTruck) LoadCargo() error {
-	e.cargo +=2
+	e.cargo += 2
 	e.battery -= 1
 	return nil
 }
 func (e *ElectricTruck) UnloadCargo() error {
-	e.cargo =0
+	e.cargo = 0
 	e.battery -= 1
 	return nil
 }
@@ -50,19 +50,16 @@ func processTruck(truck Truck) error {
 	fmt.Printf("Processing truck : %+v\n", truck)
 
 	if err := truck.LoadCargo(); err != nil {
-		return fmt.Errorf("Error loading cargo: %w", err)
+		return fmt.Errorf("error loading cargo: %w", err)
 	}
 
 	if err := truck.UnloadCargo(); err != nil {
-		return fmt.Errorf("Error loading cargo: %w", err)
+		return fmt.Errorf("error loading cargo: %w", err)
 	}
 	return nil
 
 }
 
 func main() {
-
-
-
 
 }
